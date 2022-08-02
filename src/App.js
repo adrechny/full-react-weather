3f4a72fcdba8b679333ed9dc6fc1fb5c174a3866
adrechny <adrechny311@gmail.com>
2022-08-01 19:53:45 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Allison Drechny and is{" "}
-        <a
-          href="https://github.com/adrechny/full-react-weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Allison Drechny and is{" "}
+          <a
+            href="https://github.com/adrechny/full-react-weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
